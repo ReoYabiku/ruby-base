1ef89c5af16ccb2a64fd7baa3bc4112e521f37e7
ReoYabiku <alcremikyu.z@gmail.com>
2024-05-15 09:15:15 +0900

    rubocop

--- main.rb
+++ main.rb
@@ -1,18 +1,21 @@
+# frozen_string_literal: true
+
+# My First Ruby Class, Greeter greets for everybody.
 class Greeter
-    def initialize(name = "World")
-      @name = name
-    end
+  def initialize(name = 'World')
+    @name = name
+  end
 
-    def say_hi
-        puts "Hi #{@name}!"
-    end
+  def say_hi
+    puts "Hi #{@name}!"
+  end
 
-    def say_bye
-        puts "Bye #{@name}, come back soom."
-    end
+  def say_bye
+    puts "Bye #{@name}, come back soom."
+  end
 end
 
-if __FILE__ == $0
-    g = Greeter.new("Yabiku")
-    g.say_hi
+if __FILE__ == $PROGRAM_NAME
+  g = Greeter.new('Yabiku')
+  g.say_hi
 end
